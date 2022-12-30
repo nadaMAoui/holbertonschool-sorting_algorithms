1e4e6b3f0e983ba1d3dbe3a41574ef1ca496fdae
nadaMAoui <maaouinada747@gmail.com>
2022-12-30 09:33:19 +0100

    header file ..

--- sort.h
+++ sort.h
@@ -13,13 +13,14 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void swap(int *i, int *j);
 void bubble_sort(int *array, size_t size);
 
 #endif
