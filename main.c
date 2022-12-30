be9ecb2d233bcfe2ab481e48ecb164eb529f0704
nadaMAoui <maaouinada747@gmail.com>
2022-12-30 09:03:26 +0100

    main funct upd

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
 #include "sort.h"
 
 /**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+    size_t n = sizeof(array) / sizeof(array[0]);
+
+    print_array(array, n);
+    printf("\n");
+    bubble_sort(array, n);
+    printf("\n");
+    print_array(array, n);
+    return (0);
+}
+
+/**
  * print_array - Prints an array of integers
  *
  * @array: The array to be printed
@@ -8,17 +26,17 @@
  */
 void print_array(const int *array, size_t size)
 {
-    size_t i;
+	size_t i;
 
-    i = 0;
-    while (array && i < size)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", array[i]);
-        ++i;
-    }
-    printf("\n");
+	i = 0;
+	while (array && i < size)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", array[i]);
+		++i;
+	}
+	printf("\n");
 }
 
 /**
@@ -28,16 +46,16 @@ void print_array(const int *array, size_t size)
  */
 void print_list(const listint_t *list)
 {
-    int i;
+	int i;
 
-    i = 0;
-    while (list)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", list->n);
-        ++i;
-        list = list->next;
-    }
-    printf("\n");
+	i = 0;
+	while (list)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", list->n);
+		++i;
+		list = list->next;
+	}
+	printf("\n");
 }
