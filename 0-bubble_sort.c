9b31a2f598049ecd7b4ea371d407f9861361ebbb
nadaMAoui <maaouinada747@gmail.com>
2022-12-30 09:33:45 +0100

    bubble_sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+/**
+*swap_sort - a function switches values while sorting
+*@i: pointer to first value to switch
+*@j: pointer to second value to switch
+*Return: (void)
+*/
+void swap_sort(int *i, int *j)
+{
+	int tmp;
+
+	tmp = *i;
+	*i = *j;
+	*j = tmp;
+}
+
+/**
+ * bubble_sort - sort array
+ * @array: array to sort
+ * @size: length of array
+ * Return: (void)
+*/
+void bubble_sort(int *array, size_t size)
+{
+	int i, j;
+
+	if (array == NULL)
+		return;
+	{
+		for (i = 0; i < size; i++)
+			for (j = i + 1; j < i; j++)
+				if (array[j] > array[j + 1])
+				{
+					swap_sort(&array[j], &array[j + 1]);
+					print_array(array, size);
+				}
+	}
+}
