5825f4f7bd91e1a52b83fc717a8820e87e696bbf
nadaMAoui <maaouinada747@gmail.com>
2023-01-03 10:37:47 +0100

    selection sort-array

---
+++ 2-selection_sort.c
@@ -0,0 +1,44 @@
+#include "sort.h"
+#include <string.h>
+
+/**
+ * swap - Function swaps  two data 
+ * @a: Fisrt value
+ * @b: Second value
+ * Return: void
+ */
+void swap(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *b;
+	*b = *a;
+	*a = tmp;
+}
+void selection_sort(int *array, size_t size)
+/**
+ * selection_sort - sort aray of data by selecting each time the min
+ * @array: vctor o data to be sorted
+ * @size: n data
+ * Return: void
+*/
+{
+	unsigned int i, j, min_pos;
+
+	if (array == NULL)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_pos = i;
+		for (j = i + 1; j < size; j++)
+			if (array[j] < array[min_pos])
+				min_pos = j;
+
+		if (i != min_pos)
+		{
+			swap(&array[min_pos], &array[i]);
+			print_array(array, size);
+		}
+	}
+}
